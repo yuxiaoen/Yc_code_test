a8ccca1034e80d298ef4d13c1d8788fdfa808eba
jett.2017 <285179335@qq.com>
2017-08-10 17:19:25 +0800

    create project

---
+++ Yc_Code_test/Yc_Code_test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Yc_Code_test
+//
+//  Created by yu on 2017/8/10.
+//  Copyright © 2017年 jett. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Yc_Code_test/Yc_Code_test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Yc_Code_test
+//
+//  Created by yu on 2017/8/10.
+//  Copyright © 2017年 jett. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
