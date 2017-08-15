cad3a598fc8dc53e032c30a7253d1b0782876eac
jett.2017 <285179335@qq.com>
2017-08-15 21:31:33 +0800

    XML解析

---
+++ Yc_Code_test/Yc_Code_test/tool/XMLPrase/XMLParse.h
@@ -0,0 +1,13 @@
+//
+//  XMLParse.h
+//  Yc_Code_test
+//
+//  Created by yu on 2017/8/15.
+//  Copyright © 2017年 jett. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XMLParse : NSObject
+
+@end
---
+++ Yc_Code_test/Yc_Code_test/tool/XMLPrase/XMLParse.m
@@ -0,0 +1,13 @@
+//
+//  XMLParse.m
+//  Yc_Code_test
+//
+//  Created by yu on 2017/8/15.
+//  Copyright © 2017年 jett. All rights reserved.
+//
+
+#import "XMLParse.h"
+
+@implementation XMLParse
+
+@end
