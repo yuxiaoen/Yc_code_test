aa5396dec9b6175eafa864874b2c884ccf5af38a
jett.2017 <285179335@qq.com>
2017-08-18 20:19:57 +0800

    xml test

--- Yc_Code_test/Yc_Code_test/tool/XMLPrase/XMLParse.m
+++ Yc_Code_test/Yc_Code_test/tool/XMLPrase/XMLParse.m
@@ -23,6 +23,41 @@
     return sharedInstance;
 }
 
+-(instancetype)init{
+    self = [super init];
+    if (self) {
+        
+    }
+    return self;
+}
+
+
+-(void)parserDidStartDocument:(NSXMLParser *)parser{
+    //开始解析
+}
+
+
+-(void)parser:(NSXMLParser *)parser didStartElement:(NSString *)elementName namespaceURI:(NSString *)namespaceURI qualifiedName:(NSString *)qName attributes:(NSDictionary<NSString *,NSString *> *)attributeDict{
+    //解析节点，解析参数
+    
+}
+
+- (void)parser:(NSXMLParser *)parser foundCharacters:(NSString *)string{
+    //节点值
+    
+}
+
+
+-(void)parser:(NSXMLParser *)parser didEndElement:(NSString *)elementName namespaceURI:(NSString *)namespaceURI qualifiedName:(NSString *)qName{
+    //节点分析完毕
+    
+}
+
+
+-(void)parserDidEndDocument:(NSXMLParser *)parser{
+    //解析完毕
+    
+}
 
 
 @end
